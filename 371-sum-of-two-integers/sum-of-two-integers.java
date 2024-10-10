0f2c2c6d010663332ba11b15db9b046362489779
Ayonija Mathur <ayonija.mathur@gmail.com>
2024-10-10 21:49:26 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (66.40%) - LeetSync

---
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int getSum(int a, int b) {
+         while (b != 0) {
+            // Calculate carry
+            int carry = a & b;
+
+            // Sum without carrying
+            a = a ^ b;
+
+            // Shift carry to the left
+            b = carry << 1;
+        }
+        return a; // Final sum
+    }
+}
