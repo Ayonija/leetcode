79441bb7eb8dad1ddaa06e28ca0dae2d2fc87bea
Ayonija Mathur <ayonija.mathur@gmail.com>
2025-01-21 17:38:43 +0530

    Time: 4 ms (76.16%) | Memory: 44.8 MB (35.51%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-       
+       /*
         HashMap<Character, Integer> dict = new HashMap<Character, Integer>();
         for(char c : s.toCharArray()){
             if(dict.containsKey(c)){
@@ -25,6 +25,21 @@ class Solution {
             return false;
         }
         else
-            return true;
+            return true; */
+
+
+        // If lengths are not the same, they cannot be anagrams
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        // Convert strings to character arrays and sort them
+        char[] charArray1 = s.toCharArray();
+        char[] charArray2 = t.toCharArray();
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+        
+        // Compare sorted arrays
+        return Arrays.equals(charArray1, charArray2);
     }
 }
