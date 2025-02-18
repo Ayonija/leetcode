f880bf5c657dab8289fcfa22357ddbfdf6e18027
Ayonija Mathur <ayonija.mathur@gmail.com>
2025-02-19 00:25:35 +0530

    Time: 9 ms (88.25%) | Memory: 46.4 MB (30.38%) - LeetSync

---
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+    private static final String NULL_NODE = "null";
+    private static final String DELIMITER = ",";
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        serializeHelper(root, sb);
+        return sb.toString();
+    }
+
+    private void serializeHelper(TreeNode node, StringBuilder sb) {
+        if (node == null) {
+            sb.append(NULL_NODE).append(DELIMITER);
+            return;
+        }
+
+        sb.append(node.val).append(DELIMITER);
+        serializeHelper(node.left, sb);
+        serializeHelper(node.right, sb);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+         if (data == null || data.isEmpty()) {
+            return null;
+        }
+
+        Queue<String> nodes = new LinkedList<>(Arrays.asList(data.split(DELIMITER)));
+        return deserializeHelper(nodes);
+    }
+
+    private TreeNode deserializeHelper(Queue<String> nodes) {
+        String val = nodes.poll();
+
+        if (NULL_NODE.equals(val)) {
+            return null;
+        }
+
+        TreeNode node = new TreeNode(Integer.parseInt(val));
+        node.left = deserializeHelper(nodes);
+        node.right = deserializeHelper(nodes);
+
+        return node;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
